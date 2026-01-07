527ceaabae08d6195135f010534375cf24e49564
Benjamin Walkenhorst <krylon@posteo.de>
2026-01-07 15:21:36 +0100

    database - add packages database and database/query

---
+++ database/qinit.go
@@ -0,0 +1,38 @@
+// /home/krylon/go/src/github.com/blicero/chili/database/qinit.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 06. 01. 2026 by Benjamin Walkenhorst
+// (c) 2026 Benjamin Walkenhorst
+// Time-stamp: <2026-01-07 15:15:28 krylon>
+
+package database
+
+var qInit = []string{
+	`
+CREATE TABLE network (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    addr TEXT UNIQUE NOT NULL,
+    added INTEGER NOT NULL,
+    last_scan INTEGER NOT NULL DEFAULT 0,
+    CHECK (last_scan >= 0)
+) STRICT
+`,
+	"CREATE INDEX net_scan_idx ON network (last_scan)",
+	`
+CREATE TABLE device (
+    id INTEGER PRIMARY KEY,
+    net_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    addr TEXT UNIQUE NOT NULL,
+    added INTEGER NOT NULL,
+    last_contact INTEGER NOT NULL DEFAULT 0,
+    os TEXT,
+    class INTEGER NOT NULL,
+    FOREIGN KEY (net_id) REFERENCES network (id)
+        ON UPDATE RESTRICT
+        ON DELETE CASCADE,
+    CHECK (last_contact >= 0)
+) STRICT
+`,
+	"CREATE INDEX dev_net_idx ON device (net_id)",
+}
---
+++ database/query/query.go
@@ -0,0 +1,18 @@
+// /home/krylon/go/src/github.com/blicero/chili/database/query/query.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 07. 01. 2026 by Benjamin Walkenhorst
+// (c) 2026 Benjamin Walkenhorst
+// Time-stamp: <2026-01-07 15:19:09 krylon>
+
+package query
+
+//go:generate stringer -type=ID
+
+// ID signifies a particular SQL query we perform on the database.
+type ID uint8
+
+const (
+	NetAdd ID = iota
+	NetUpdateLastScan
+	DeviceAdd
+)
