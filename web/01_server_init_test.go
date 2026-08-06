fc309390aa0b8ea2eed2937590c6ca54fff7003c
Benjamin Walkenhorst <krylon@posteo.de>
2026-08-06 10:56:18 +0200

    web - update call to Create in test

--- web/01_server_init_test.go
+++ web/01_server_init_test.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 25. 08. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2026-06-03 11:17:54 krylon>
+// Time-stamp: <2026-08-06 10:51:07 krylon>
 
 package web
 
@@ -17,7 +17,7 @@ func TestServerCreate(t *testing.T) {
 
 	addr = fmt.Sprintf("[::1]:%d", testPort)
 
-	if srv, err = Create(addr); err != nil {
+	if srv, err = Create(addr, nil); err != nil {
 		srv = nil
 		t.Fatalf("Error creating Server: %s",
 			err.Error())
