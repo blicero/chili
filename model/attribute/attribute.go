04185a752f177f2110a6a6787a9b753074877825
Benjamin Walkenhorst <krylon@posteo.de>
2026-08-08 14:46:50 +0200

    attribute - introduce function attribute.Parse

--- model/attribute/attribute.go
+++ model/attribute/attribute.go
@@ -2,9 +2,14 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 11. 07. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-08-04 09:48:55 krylon>
+// Time-stamp: <2026-08-08 11:22:07 krylon>
 
 package attribute
+
+import (
+	"fmt"
+	"strings"
+)
 
 //go:generate stringer -type=ID
 
@@ -19,3 +24,23 @@ const (
 	SNMP
 	Services
 )
+
+// Parse attempts to convert the given string to an attribute ID.
+func Parse(s string) ID {
+	switch strings.ToLower(s) {
+	case "updates":
+		return Updates
+	case "diskspace":
+		return DiskSpace
+	case "uptime":
+		return Uptime
+	case "packages":
+		return Packages
+	case "snmp":
+		return SNMP
+	case "services":
+		return Services
+	default:
+		panic(fmt.Sprintf("Unknown attribute ID %s", s))
+	}
+} // func Parse(s string) ID
