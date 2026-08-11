3c8667539c0f048e0d0a66994376f848eeeaff2f
Benjamin Walkenhorst <krylon@posteo.de>
2026-08-11 12:41:52 +0200

    database - change ordering in show_dmi.sql

--- database/show_dmi.sql
+++ database/show_dmi.sql
@@ -1,5 +1,5 @@
 -- /home/krylon/go/src/github.com/blicero/chili/database/show_dmi.sql
--- Time-stamp: <2026-08-10 14:03:10 krylon>
+-- Time-stamp: <2026-08-10 21:37:37 krylon>
 -- created on 10. 08. 2026 by Benjamin Walkenhorst
 -- (c) 2026 Benjamin Walkenhorst
 -- Use at your own risk!
@@ -13,4 +13,4 @@ SELECT
 FROM attribute a
 INNER JOIN device d ON a.dev_id = d.id
 WHERE a.atype = 6
-ORDER BY a.timestamp DESC;
+ORDER BY d.name;
