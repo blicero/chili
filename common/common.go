6d1a95c1d1b9316658194713a420cdfd3c5209d8
Benjamin Walkenhorst <krylon@posteo.de>
2026-08-14 12:38:15 +0200

    bump version to 0.6.0

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 23. 07. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2026-08-13 10:51:22 krylon>
+// Time-stamp: <2026-08-14 12:38:08 krylon>
 
 // Package common contains definitions used throughout the application
 package common
@@ -35,7 +35,7 @@ import (
 // application.
 const (
 	AppName                  = "Chili"
-	Version                  = "0.5.0"
+	Version                  = "0.6.0"
 	DefaultPort              = 4001
 	TimestampFormatMinute    = "2006-01-02 15:04"
 	TimestampFormat          = "2006-01-02 15:04:05"
